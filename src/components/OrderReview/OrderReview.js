45b7aea5c715af946dcf962066608d2a2c97da0b
rinanasir <rinanasir25@gmail.com>
2021-10-03 14:08:31 +0600

    created hooks

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
+    const [products] = useProducts();
     return (
         <div>
             <h2>this is order review</h2>
+            <h1>{products.length}</h1>
         </div>
     );
 };
