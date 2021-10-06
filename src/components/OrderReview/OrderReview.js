6d74dfed4b364829b58f436af7d851d17f277314
rinanasir <rinanasir25@gmail.com>
2021-10-06 16:57:22 +0600

    removed from localDB

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -3,14 +3,18 @@ import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { deleteFromDb } from '../../utilities/fakedb';
 
 const OrderReview = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
     const handleRemove = key => {
         const newCart = cart.filter(product => product.key !== key);
         setCart(newCart);
+        deleteFromDb(key);
     }
+
     return (
         <div className="shop-container">
             <div className="product-container">
